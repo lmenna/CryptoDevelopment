40cbbf39e32fc7a7f968d3b1c22ac9d1dd712d1d
lmenna <louismenna@yahoo.com>
2018-09-24 15:41:43 -0400

    Create a balance map to store token balances in each account. Add unit testing for all cases.  100% coverage.

--- contracts/SampleToken.sol
+++ contracts/SampleToken.sol
@@ -2,9 +2,22 @@ pragma solidity ^0.4.2;
 
 contract SampleToken {
 
+  // ERC20 variables
+  // name is ERC20 optional
+  string public name = "Sample Token";
+  // symbol is ERC20 optional
+  string public symbol = "SAM";
+
+  // Not ERC20
+  // standard is not part of ERC20
+  string public standard = "Sample Token v1.0";
   uint256 public totalSupply;
 
+  mapping(address => uint256) public balanceOf;
+
   function SampleToken (uint256 _initialSupply) public {
+    // allocate the initial supply
+    balanceOf[msg.sender] = _initialSupply;
     totalSupply = _initialSupply;
   }
 }
--- test/SampleToken.js
+++ test/SampleToken.js
@@ -1,13 +1,33 @@
 var SampleToken = artifacts.require("./SampleToken.sol");
 
 contract('SampleToken', function(accounts) {
+  var tokenInstance;
 
-  it('sets the total supply upon deployment', function() {
+  it('Initializes the contract with the correct values', function() {
+      return SampleToken.deployed().then(function(instance) {
+        tokenInstance = instance;
+        return tokenInstance.name();
+      }).then(function(name) {
+        assert.equal(name, 'Sample Token', 'has the correct name')
+        return tokenInstance.symbol();
+      }).then(function(symbol) {
+        assert.equal(symbol, 'SAM', 'has the correct symbol');
+        return tokenInstance.standard();
+      }).then(function(standard) {
+        assert.equal(standard, 'Sample Token v1.0', 'has the correct standard');
+      });
+  });
+
+
+  it('Allocates the initial token supply upon deployment', function() {
     return SampleToken.deployed().then(function(instance) {
       tokenInstance = instance;
       return tokenInstance.totalSupply();
     }).then(function(totalSupply) {
-      assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to 1,000,000');
+      assert.equal(totalSupply.toNumber(), 1000000, 'Expected total supply to be 1,000,000');
+      return tokenInstance.balanceOf(accounts[0]);
+    }).then(function(adminBalance) {
+      assert.equal(adminBalance.toNumber(), 1000000, 'Expected initial supply to be in the admin account')
     });
   });
-})
+});
