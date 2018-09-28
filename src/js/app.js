cba48978c58f6be8395135bca22f6c56854b8652
lmenna <louismenna@yahoo.com>
2018-09-28 16:02:15 -0400

    Connected the wepage UI to the blockchain using web3.  Displaying the account Address on the page

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 App = {
   web3Provider: null,
   contracts: {},
+  account: '0x0',
 
   init: function() {
     console.log("App initialized...")
@@ -24,13 +25,32 @@ App = {
   initContracts: function() {
     console.log("initContracts");
     $.getJSON("SampleTokenSale.json", function(SampleTokenSale) {
-       App.contracts.SampleTokenSale = TruffleContract(SampleTokenSale);
-       App.contracts.SampleTokenSale.setProvider(App.web3Provider);
-       App.contracts.SampleTokenSale.deployed().then(function(SampleTokenSale) {
+        App.contracts.SampleTokenSale = TruffleContract(SampleTokenSale);
+        App.contracts.SampleTokenSale.setProvider(App.web3Provider);
+        App.contracts.SampleTokenSale.deployed().then(function(SampleTokenSale) {
          console.log("Sample Token Sale Address:", SampleTokenSale.address);
        });
+     }).done(function() {
+       $.getJSON("SampleToken.json", function(SampleToken) {
+          App.contracts.SampleToken = TruffleContract(SampleToken);
+          App.contracts.SampleToken.setProvider(App.web3Provider);
+          App.contracts.SampleToken.deployed().then(function(SampleToken) {
+          console.log("Sample Token Address:", SampleToken.address);
+          });
+      });
     });
-  }
+    return App.render();
+},
+
+render: function() {
+  // Load account data
+  web3.eth.getCoinbase(function(err, account) {
+    if(err === null) {
+      App.account = account;
+      $('#accountAddress').html("Your Account:" + account);
+    }
+  })
+}
 }
 
 $(function() {
