653d58fcb1fb05b6e81e4c5affb616e6aa08fc26
lmenna <louismenna@yahoo.com>
2018-09-28 13:12:04 -0400

    Updated UI and added connection to Token Sale Contract using web3 in javascript.

---
+++ src/js/app.js
@@ -0,0 +1,40 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+
+  init: function() {
+    console.log("App initialized...")
+    return App.initWeb3();
+  },
+
+  initWeb3: function() {
+    console.log("initWeb3");
+    if (typeof web3 !== 'undefined') {
+      // If a web3 instance is already provided by Meta Mask.
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // Specify default instance if no web3 instance provided
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+    }
+    return App.initContracts();
+  },
+
+  initContracts: function() {
+    console.log("initContracts");
+    $.getJSON("SampleTokenSale.json", function(SampleTokenSale) {
+       App.contracts.SampleTokenSale = TruffleContract(SampleTokenSale);
+       App.contracts.SampleTokenSale.setProvider(App.web3Provider);
+       App.contracts.SampleTokenSale.deployed().then(function(SampleTokenSale) {
+         console.log("Sample Token Sale Address:", SampleTokenSale.address);
+       });
+    });
+  }
+}
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  })
+});
